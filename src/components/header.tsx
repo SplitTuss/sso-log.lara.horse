1d349b05d5a0a7c3169d4990ff1886140220d95d
Lara <lolaviras@gmail.com>
2025-07-11 09:28:19 -0700

    added new link to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ export function Header() {
   return (<>
     <ol className="bg-pink-500 flex items-center justify-center">
       <li className="p-10 text-white"><a href="https://lara.horse">lara.horse</a></li>
+      <li className="p-10 text-white"><a href="https://horses.lara.horse">real horses</a></li>
       <li className="p-10 text-white"><a href="https://laracraft.io">laracraft.io</a></li>
       <li className="p-10 text-white"><a href="https://ssodb.bplaced.net/db/en/horses.html">StarStableDatabase</a></li>
       {/* <li className="p-10 text-purple-900">sso-log.lara.horse</li>
