8e4df3844bdd475cdc58aad612ba4a90d9f3914a
Lara <lolaviras@gmail.com>
2025-08-04 11:39:11 -0700

    removed links and old footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,26 @@
 import Header from "@/components/header";
-//import { Horses } from "@/components/horses";
+import { Horses } from "@/components/horses";
 
 //const imageLink = 'https://s3.us-east-1.amazonaws.com/images.sso-log.lara.horse/'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <>
       <Header />
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <section>
-          <p>first off: the images are not mine! </p>
-          <p>I will be forever grateful to Abbie Starwatcher,</p>
-          <p>who has from the beginning volunteered her time to take photos</p>
-          <p>of every single horse in this game.</p>
-          <p>I have linked the SSO Database in the header.</p>
-        </section>
-        {/*<Horses />*/}
-        {/*<img src={imageLink + 'akhal-teke/gen1-1.webp'} alt="hier"/>*/}
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://lara.horse"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            lara.horse
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://laracraft.io"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            laracraft.io
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          <section>
+            <p>first off: the images are not mine! </p>
+            <p>I will be forever grateful to Abbie Starwatcher,</p>
+            <p>who has from the beginning volunteered her time to take photos</p>
+            <p>of every single horse in this game.</p>
+            <p>I have linked the SSO Database in the header.</p>
+          </section>
+          <Horses />
+          {/*<img src={imageLink + 'akhal-teke/gen1-1.webp'} alt="hier" />*/}
+        </main>
+      </div>
+    </>
   );
 }
