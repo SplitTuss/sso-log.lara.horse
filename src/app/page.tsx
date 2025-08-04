aaabb1c00e9ddbf86cd1b59476a6e27d237a8b2a
Lara <lolaviras@gmail.com>
2025-08-04 14:21:00 -0700

    prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/header";
-import { Horses } from "@/components/horses";
+import Header from '@/components/header';
+import { Horses } from '@/components/horses';
 
 //const imageLink = 'https://s3.us-east-1.amazonaws.com/images.sso-log.lara.horse/'
 
@@ -8,12 +8,11 @@ export default function Home() {
     <>
       <Header />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <section>
             <p>first off: the images are not mine! </p>
             <p>I will be forever grateful to Abbie Starwatcher,</p>
-            <p>who has from the beginning volunteered her time to take photos</p>
+            <p>who has from the beginning volunteered (I think) her time to take photos</p>
             <p>of every single horse in this game.</p>
             <p>I have linked the SSO Database in the header.</p>
           </section>
