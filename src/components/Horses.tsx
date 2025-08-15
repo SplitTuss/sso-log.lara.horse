c1c76cd52c37ff255f86fd2db6ec1f8b91326568
Lara <lolaviras@gmail.com>
2025-08-15 14:29:06 -0700

    update horseId key

--- src/components/Horses.tsx
+++ src/components/Horses.tsx
@@ -15,13 +15,13 @@ export function Horses() {
 
                 <ul className="flex flex-row flex-wrap gap-10">
                   {generation.colors.map((color, index) => {
-                    const key = `${horse.breed}-${generation.id}-${color.name}`;
+                    const horseId = `${horse.breed}-gen${generation.id}-color${index}`;
 
                     return (
-                      <li key={`${key}-${index}`} className="size-40 border-4 flex-col">
-                        <img className="h-20 mx-auto py-2" src={color.imageUrl} alt={key} />
+                      <li key={horseId} className="size-40 border-4 flex-col">
+                        <img className="h-20 mx-auto py-2" src={color.imageUrl} alt={horseId} />
 
-                        <EditHorsePopup dbKey={key} />
+                        <EditHorsePopup horseId={horseId} />
                       </li>
                     );
                   })}
