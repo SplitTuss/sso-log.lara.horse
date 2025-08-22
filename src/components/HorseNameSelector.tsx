d7938385057502a788f97aa37a4ecd33ba4fc8c7
Lara <lolaviras@gmail.com>
2025-08-22 16:04:55 -0700

    created HorseNameSelector ComboBox

---
+++ src/components/HorseNameSelector.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { Check, ChevronsUpDown } from 'lucide-react';
+
+import { HORSE_NAMES } from '@/data/horseNames';
+import { cn } from '@/utils';
+import { Button } from './Button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from './Command';
+import { Popover, PopoverContent, PopoverTrigger } from './Popover';
+
+export function HorseNameSelector() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<null | string>(null);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button variant="outline">
+          {typeof value === 'string' ? value : 'Select name'}
+
+          <ChevronsUpDown className="opacity-50" />
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search name..." />
+          <CommandList>
+            <CommandEmpty>No names found.</CommandEmpty>
+            <CommandGroup>
+              {HORSE_NAMES.first.map((name, index) => (
+                <CommandItem
+                  key={index}
+                  value={name}
+                  onSelect={(newValue) => {
+                    setValue(newValue === value ? null : newValue);
+                    setOpen(false);
+                  }}
+                >
+                  {name}
+
+                  <Check className={cn('ml-auto', name === value ? 'opacity-100' : 'opacity-0')} />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
