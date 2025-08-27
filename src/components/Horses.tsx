aa17a7987f7a8fb47aa6bead80b12d9e3f8c15b2
Lara <lolaviras@gmail.com>
2025-08-27 11:06:53 -0700

    styled horses

--- src/components/Horses.tsx
+++ src/components/Horses.tsx
@@ -3,23 +3,23 @@ import { HorseOwners } from './HorseOwners';
 
 export function Horses() {
   return (
-    <ul>
+    <ul className="flex flex-col gap-6">
       {HORSE_DATA.map((horse, index) => (
         <li key={index}>
-          <div className="text-primary">{horse.breed}</div>
+          <div className="text-primary text-2xl">{horse.breed}</div>
 
-          <ul>
+          <ul className="flex flex-col gap-6">
             {horse.generations.map((generation, index) => (
               <li key={index}>
-                <div className="text-primary">{generation.id}</div>
+                <div className="text-primary pb-2">{generation.id}. generation</div>
 
-                <ul className="flex flex-row flex-wrap gap-10">
+                <ul className="flex flex-row flex-wrap gap-6">
                   {generation.colors.map((color, index) => {
                     const horseId = `${horse.breed}-gen${generation.id}-color${index}`;
 
                     return (
                       <li key={horseId} className="border-4 flex-col rounded-xl">
-                        <img className="h-20 mx-auto py-2" src={color.imageUrl} alt={horseId} />
+                        <img className="h-25 mx-auto py-2" src={color.imageUrl} alt={horseId} />
 
                         <HorseOwners horseId={horseId} />
                       </li>
