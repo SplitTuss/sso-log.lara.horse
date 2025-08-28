138f2f20ffa96488eb3e8daf4d13c8694149057c
Lara <lolaviras@gmail.com>
2025-08-28 11:05:39 -0700

    changed focus ring to only show when dropdowns are open

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -8,7 +8,16 @@ function Popover({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Root
 }
 
 function PopoverTrigger({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Trigger>) {
-  return <PopoverPrimitive.Trigger data-slot="popover-trigger" {...props} />;
+  return (
+    <PopoverPrimitive.Trigger
+      data-slot="popover-trigger"
+      className={`
+        data-[state=open]:border-ring data-[state=open]:ring-ring/50 data-[state=open]:ring-[3px]
+        focus-visible:ring-0 focus-visible:border-1 focus-visible:border-input
+      `}
+      {...props}
+    />
+  );
 }
 
 function PopoverContent({
@@ -24,7 +33,12 @@ function PopoverContent({
         align={align}
         sideOffset={sideOffset}
         className={cn(
-          'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-md border p-4 shadow-md outline-hidden',
+          `
+            bg-popover text-popover-foreground
+            z-50 w-72 origin-(--radix-popover-content-transform-origin)
+            rounded-md border p-4 shadow-md outline-hidden
+            data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2
+          `,
           className,
         )}
         {...props}
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/** this combines any number of className strings or style objects into one className string */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
