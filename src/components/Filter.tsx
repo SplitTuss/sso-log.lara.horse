37269be97cde3979402a035072302d2933d367ce
Lara <lolaviras@gmail.com>
2025-09-17 13:53:05 -0700

    hide second checkbox for now

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,29 +1,53 @@
 import { FunnelIcon } from 'lucide-react';
 import { Checkbox } from './Checkbox';
+import { useState } from 'react';
+import { cn } from '@/utils';
 
 interface FilterProps {
   onHideUnavailable: (value: boolean) => void;
+  onHideNotOwned: (value: boolean) => void;
 }
 
-export const Filter = ({ onHideUnavailable }: FilterProps) => {
-  return (
-    <>
-      <div className="flex flex-row items-center">
-        <FunnelIcon className="text-muted-foreground mr-2" size={20} />
-      </div>
+export const Filter = ({ onHideUnavailable, onHideNotOwned }: FilterProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-      <div className="flex flex-row items-center gap-2">
-        Hide unavailable:
-        <Checkbox
-          onCheckedChange={(value) => {
-            if (value === 'indeterminate') {
-              onHideUnavailable(false);
-            } else {
-              onHideUnavailable(value);
-            }
-          }}
-        />
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div className="text-center border-1 rounded-xl">
+      <button className="hover:cursor-pointer" onClick={toggleExpand}>
+        <FunnelIcon className="text-muted-foreground mr-2" size={20} />
+      </button>
+
+      <div className={cn('items-center gap-2', !isExpanded && 'hidden')}>
+        <div>
+          Hide unavailable:
+          <Checkbox
+            onCheckedChange={(value) => {
+              if (value === 'indeterminate') {
+                onHideUnavailable(false);
+              } else {
+                onHideUnavailable(value);
+              }
+            }}
+          />
+        </div>
+
+        {/* <div>
+          only show owned:
+          <Checkbox
+            onCheckedChange={(value) => {
+              if (value === 'indeterminate') {
+                onHideNotOwned(false);
+              } else {
+                onHideNotOwned(value);
+              }
+            }}
+          />
+        </div> */}
       </div>
-    </>
+    </div>
   );
 };
