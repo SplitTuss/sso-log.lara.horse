314ec7191b29284a7655482a96c214077fffae9c
Lara <lolaviras@gmail.com>
2026-02-12 16:52:45 -0700

    made logo in header link to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,13 @@ import { Button } from './shadcn/Button';
 export default function Header() {
   return (
     <div className="bg-accent flex items-center justify-between ">
-      <img
-        src="/logo.svg"
-        alt="sso-log.lara.horse.io logo"
-        className="w-20 h-20 rounded-lg ml-2 sm:ml-4"
-      />
+      <Link href="/">
+        <img
+          src="/logo.svg"
+          alt="sso-log.lara.horse.io logo"
+          className="w-20 h-20 rounded-lg ml-2 sm:ml-4"
+        />
+      </Link>
       <div className="flex justify-center gap-6 py-4 mr-2 sm:mr-4">
         <Link href="https://horses.lara.horse">
           <Button variant="default">real horses</Button>
