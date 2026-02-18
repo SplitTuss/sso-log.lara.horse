31ddff6b5158451e67a46fd8359a23287696446b
Lara <lolaviras@gmail.com>
2026-02-18 15:37:40 -0700

    removed header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,21 +12,12 @@ export default function Header() {
           className="w-20 h-20 rounded-lg ml-2 sm:ml-4"
         />
       </Link>
-      <div className="flex justify-center gap-6 py-4 mr-2 sm:mr-4">
-        <Link href="https://horses.lara.horse">
-          <Button variant="default">real horses</Button>
-        </Link>
-        <div className="hidden sm:block">
-          <Link href="https://laracraft.io">
-            <Button variant="default">laracraft.io</Button>
-          </Link>
-        </div>
-        <Link href="https://ssodb.bplaced.net/db/en/horses.html">
-          <Button className="text-pink-500" variant="default">
-            original SSO-DB
-          </Button>
-        </Link>
-      </div>
+
+      <Link href="https://ssodb.bplaced.net/db/en/horses.html" className="mr-4">
+        <Button className="text-pink-500" variant="default">
+          original SSO-DB
+        </Button>
+      </Link>
     </div>
   );
 }
